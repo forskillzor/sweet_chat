5776f01269c090a5ba4651bdcd58ea9ba7c71db5
Sergey Orlov <skillzorwispa@gmail.com>
2020-04-01 19:22:56 +0700

    some initial implementation

--- src/ws-client.js
+++ src/ws-client.js
@@ -14,6 +14,43 @@ loginPrompt.question('login: ', l => {
   session(login)
 })
 
+const encode = obj => JSON.stringify(obj)
+const decode = str => JSON.parse(str)
+
+const command = (command, value) => encode({
+  commandName: command,
+  commandValue: value
+})
+
+// TODO move from server to facade and separate to server
+const server = {
+  commands: {
+    signin: 'signin',
+    sendMessage: 'message',
+    connectToUser: 'connectToUser',
+    createRoom: 'createRoom',
+    changeRoom: 'changeRoom'
+  },
+  setConnection: function (connection) {
+    this.connection = connection
+  },
+  signin: function (user){
+    this.user = user
+    this.connection.send(command(this.commands.signin, this.user))
+  },
+  call: function (user) {
+    this.connection.send(command(this.commands.connectToUser, user))
+  },
+  chroom: function (room) {
+    this.connection.send(command(this.commands.changeRoom, room))
+  },
+  sendMessage: function (text) {
+    this.connection.send(command(this.commands.sendMessage, text))
+  },
+  createRoom: function (roomName) {
+    this.connection.send(command(this.commands.createRoom, roomName))
+  },
+}
 
 function session(login) {
   const chat = readline.createInterface({
@@ -26,7 +63,8 @@ function session(login) {
   const connection = new WebSocket(url)
 
   connection.onopen = () => {
-    connection.send(JSON.stringify({username: login}))
+    server.setConnection(connection)
+    server.signin(login)
   }
 
   connection.onerror = error => {
@@ -34,15 +72,38 @@ function session(login) {
   }
 
   connection.on('message', message => {
-    console.log(`server: ${message}`);
+    const decoded = decode(message)
+    if (decoded.commandName === 'message')
+      console.log(`${decoded.author} : ${decoded.commandValue}`);
   })
 
   const chatCycle = () => {
     chat.prompt()
     chat.on('line', message => {
-      connection.send(JSON.stringify({message}))
+
+      parseLine(message)
       chat.prompt()
     })
   }
   setTimeout(chatCycle, 1000)
+}
+
+/*
+ * @ command parser
+ */
+
+function parseLine(str) {
+  if (str[0] === '@'){
+    const command = str.slice(1, str.indexOf('('))
+    const param = str.slice(str.indexOf('(') + 1, str.indexOf(')'))
+    //console.log('parsed command: ', command, 'param: ', param)
+    try {
+      server[command](param)
+    }
+    catch (e){
+      console.log('not a command');
+    }
+  } else {
+    server.sendMessage(str)
+  }
 }
--- src/ws-server.js
+++ src/ws-server.js
@@ -3,36 +3,115 @@
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8081 })
 const users = {}
+const rooms = {}
 const rl = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: 'chat > '
 })
 
-wss.on('connection', ws => {
-  let username = '';
+const encode = obj => JSON.stringify(obj)
+const decode = str => JSON.parse(str)
+  
+const command = (command, value, author) => encode({
+  commandName: command,
+  commandValue: value,
+  author: author
+})
+
+const commands = {
+  signin: function (username, user) {
+    user.name = username
+    users[username] = user
+    user.rooms = []
+    console.log('signin:', users[username].name)
+  },
+  message: function (text, user) {
+    if (user.currentRoom) {
+    console.log(user.name,':', user.currentRoom, ':', text)
+    rooms[user.currentRoom].forEach((item) => {
+      if (item.name !== user.name) {
+        item.connection.send(command('message', text, user.name))
+      }
+    })
+    } else {
+      user.connection.send(command(
+        'message', 
+        'no rooms',
+        'server'
+      ))
+    }
+  },
+  connectToUser: function (toUser, user) {
+    if (user.currentRoom) {
+      if (user.name === rooms[user.currentRoom][0].name){
+        if (users[toUser]) {
+          rooms[user.currentRoom].push(users[toUser])
+          users[toUser].currentRoom = user.currentRoom
+        } else {
+          user.connection.send(command(
+            'message',
+            'no requested user',
+            'server'
+          ))
+        }
+      } else {
+        user.connection.send(command(
+          'message',
+          `you're not owner of this channel: ${user.currentRoom}`,
+          'server'
+        ))
+      }
+    } else {
+      user.connection.send(command(
+        'message',
+        'you\'re have no current room, create room please',
+        'server'
+      ))
+    }
+  },
+  createRoom: function (roomName, user) {
+    if (!rooms[roomName]){
+      rooms[roomName] = [user]
+      users[user.name].rooms.push(roomName)
+      user.currentRoom = roomName
+      console.log(`${user.name} create room '${roomName}'`)
+      user.connection.send(command(
+        'message',
+        `created room: ${roomName}`,
+        'server'
+      ))
+    } else {
+      user.connection.send(command(
+        'message',
+        `already exist room: ${roomName}`,
+        'server'
+      ))
+    }
+  },
+  changeRoom: function (roomName, user) {
+    user.currentRoom = roomName
+  }
+}
+
+wss.on('connection', function(ws) {
+  const user = { name: '', connection: ws, currentRoom: null }
   ws.on('message', message => {
     const decodedMessage = JSON.parse(message)
-    if (decodedMessage.hasOwnProperty('username')) {
-      if ( ! (decodedMessage.username in users)) {
-        users[decodedMessage.username] = null
-        username = decodedMessage.username
-        console.log('added new user: ', decodedMessage.username)
-        console.log('Users are: ', users)
-      }
-    }
-    else if (decodedMessage.hasOwnProperty('message')) {
-      console.log(`${username}: ${decodedMessage.message}`)
-    }
+    commands[decodedMessage.commandName](decodedMessage.commandValue, user)
   })
-  ws.send(`connected to server`)
+  ws.send(command(
+    'message',
+    'connected',
+    'server'
+  ))
   setTimeout(chat.bind(null, ws), 1000)
 })
 
 const chat = (ws) => {
   rl.prompt()
   rl.on('line', message => {
-    ws.send(message)
+    ws.send('message', message, 'server')
     rl.prompt()
   })
 }
